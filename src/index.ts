40ff57d392cdc850f48e258dc5154d3ec3770e4f
城管不管 <1813199657@qq.com>
2024-05-13 20:51:23 +0800

    江南

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+ 
+import prompts from "prompts";//处理终端输入
+import path from "node:path";//处理文件路径
+import fs from "node:fs";//处理文件
+import chalk from "chalk";//处理颜色
+const bootstrap = async () => {
+    console.log(`
+    ████████  ██     ██       ███████  
+    ██░░░░░░██░██    ████     ██░░░░░██ 
+   ██      ░░ ░██   ██░░██   ██     ░░██
+  ░██         ░██  ██  ░░██ ░██      ░██
+  ░██    █████░██ ██████████░██      ░██
+  ░░██  ░░░░██░██░██░░░░░░██░░██     ██ 
+   ░░████████ ░██░██     ░██ ░░███████  
+    `);
+    console.log("一GIAO窝里GIAO GIAO      GIAO")
+    const result =  await prompts([
+        {
+            type: "text",
+            name: "projectName",
+            message: "请输入项目名称:"
+        },
+    ]);
+    //管理控制台输入
+    //————————————————————————————————————————————————————————————————————————————————————————————————
+    const targetPath = path.resolve(process.cwd(), result.projectName);
+    const sourcePath = path.resolve(__dirname, "../template");
+    // Copy files from sourcePath to targetPath
+    fs.cpSync(sourcePath, targetPath, { recursive: true });
+    // Update package.json
+    const packageJsonPath = path.resolve(targetPath, 'package.json');
+    const packageJson = require(packageJsonPath);
+    packageJson.name = result.projectName;
+    // Write the updated package.json back to the file
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    // Rename _gitignore to .gitignore
+    fs.renameSync(
+        path.resolve(targetPath, "_gitignore"),
+        path.resolve(targetPath, ".gitignore")
+    );
+    //————————————————————————————————————————————————————————————————————————————————————————————————
+    //最后输出
+    console.log(`
+    cd ${result.projectName}
+    pnpm i
+    pnpm run dev
+    `)
+};
+bootstrap();
