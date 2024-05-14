ac98aa2295ddc2a13b015351a2f1ef3f3ce162b9
城管不管 <1813199657@qq.com>
2024-05-14 19:51:40 +0800

    wefwqfw

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ const bootstrap = async () => {
     console.log("一GIAO 我哩 GIAO GIAO    GIAO~")
     console.log("你真的懂唯一    ~的定义")
     console.log("缠住吻住春风吹住我吗      缠住吻住郁金香是你吗")
+    console.log("转圈圈 转圈圈  一直转圈圈")
     const result =  await prompts([
         {
             type: "text",
